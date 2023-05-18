5b1b78aa58e836f53b18bac0c824254d1bb44db0
manuel <120762787+jomaso23@users.noreply.github.com>
2023-05-18 10:20:40 -0300

    primer archivo subido a mi repositorio
    este es un archivo java seleccionado de forma random

---
+++ Calculadora.java
@@ -0,0 +1,40 @@
+/**
+ * Calculadora
+ */
+public class Calculadora {
+    /**
+     * método que suma
+     * @param num1
+     * @param num2
+     */
+    public void sumar(int num1, int num2){
+        System.out.println(num1+num2);
+    }
+
+    /**
+     * método que resta
+     * @param num1
+     * @param num2
+     */
+    public void restar(int num1, int num2){
+        //TODO hacer restar
+    }
+
+    /**
+     * método que multiplica
+     * @param num1
+     * @param num2
+     */
+    public void multiplicar(int num1, int num2){
+        //TODO hacer multiplicar
+    }
+
+    /**
+     * método que dividir
+     * @param num1
+     * @param num2
+     */
+    public void dividir(int num1, int num2){
+        //TODO hacer dividir
+    }
+}
